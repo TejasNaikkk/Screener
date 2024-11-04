5ffa79586a251d0bf34f1b749287e19b156adb08
Tejas Naik <tejas@inovsion.in>
2024-11-04 23:14:53 +0530

    intraday to daily aggregator file

---
+++ intradayDataCombiner.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+def combiner(data):
+    x = data.json()
+    y = x.get('data').get('candles')
+    df = pd.DataFrame(y, columns=['datetime', 'open', 'high', 'low', 'close', 'volume', 'openinterest'])
+
+    # Convert the datetime column to datetime objects and set it as index
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    df.set_index('datetime', inplace=True)
+
+    # Group by date and aggregate to calculate daily OHLC
+    daily_ohlc = df.resample('D').agg({
+        'open': 'first',
+        'high': 'max',
+        'low': 'min',
+        'close': 'last',
+        'volume': 'sum'
+    })
+
+    print(daily_ohlc)
